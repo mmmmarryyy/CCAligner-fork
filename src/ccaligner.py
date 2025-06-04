7eaed89211b725eb0ecd18049a8702747db930ac
Maria Barkovskaya <barkovskaya.maria@mail.ru>
2025-06-04 21:24:09 +0300

    write results to file

--- src/ccaligner.py
+++ src/ccaligner.py
@@ -20,7 +20,9 @@ elif language == 'java':
 else:
     raise ValueError(f"Unsupported language: {language}")
 
-pretty_loc = "./data/normalized_codebases/"
+print(f"language = {language}")
+
+pretty_loc = "./normalized_codebases/"
 if os.path.exists(pretty_loc):
     rmtree(pretty_loc)
 
@@ -32,9 +34,16 @@ final_dir = pretty_loc + codebase_loc.split('/')[-1] + '/' + 'obfuscated'
 
 cca = CCalignerAlgorithm(final_dir, lang_ext, 3, 0, query_file=query_file)
 pairs = cca.run_algo()
+
+output_filename = "results/result.txt"
+if not os.path.exists("results"):
+    os.makedirs("results")
+
 for file1, file2 in pairs:
     file_name1 = file1.split('/')[-2]
     file_name2 = file2.split('/')[-2]
     fragment1 = file1.split('/')[-1][:-3]
     fragment2 = file2.split('/')[-1][:-3]
     print(f"{file_name1} and {file_name2} contain codeclone in lines {fragment1} and {fragment2} respectively")
+    with open(output_filename, 'a+') as fileOut:
+        fileOut.write(f"{file1},{file2}\n")
